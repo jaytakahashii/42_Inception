160c7d2edb2893db05d217318291f6c843ee8c05
Jay Takahashi <qsakura14555@gmail.com>
2025-06-04 14:49:59 +0900

    feat(SRC): ✨  cert to secrets

--- srcs/requirements/nginx/tools/start.sh
+++ srcs/requirements/nginx/tools/start.sh
@@ -3,10 +3,8 @@
 # TLS証明書がない場合は自己署名証明書を生成
 if [ ! -f /etc/nginx/certs/nginx.crt ]; then
   mkdir -p /etc/nginx/certs
-  openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -subj "/C=JP/ST=Tokyo/L=Tokyo/O=42Tokyo/CN=jtakahas.42.fr" \
-    -keyout /etc/nginx/certs/nginx.key \
-    -out /etc/nginx/certs/nginx.crt
+  cp "$NGINX_CERT_FILE" /etc/nginx/certs/nginx.crt
+  cp "$NGINX_KEY_FILE" /etc/nginx/certs/nginx.key
 fi
 
 # Nginx起動
